1952fcda8c921631d235f4989de5051dd99c35cc
zhanglan <delylaric@gmail.com>
2018-10-08 17:00:06 +0800

    app home topbar && notifications

---
+++ src/app/home/Topbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="app-topbar level is-marginless">
+
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'AppTopbar'
+}
+</script>
--- src/app/home/index.vue
+++ src/app/home/index.vue
@@ -1,24 +1,32 @@
 <template>
-  <div>
-    homepage
-    <router-view/>
+  <div style="display: flex; flex-flow: column; height: 100vh">
+    <Topbar />
+    <Notifications ref="notifications"/>
+    <div style="flex: 2; overflow: auto;">
+      <router-view
+        :notifications="$refs.notifications"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Topbar from './Topbar'
 import tokenManager from './tokenManager'
+import Notifications from '@/components/notifications'
 
 export default {
   mixins: [tokenManager],
 
-  data () {
-    return {
-      token: {}
-    }
+  components: {
+    Topbar,
+    Notifications
   },
 
-  methods: {
-
+  data () {
+    return {
+      token: {},
+    }
   }
 }
 </script>
