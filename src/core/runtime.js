4771ff9476a159ae55ebc125aa6f7f076e1ddd76
zhanglan <delylaric@gmail.com>
2018-10-12 22:33:09 +0800

    add runtime module in core

---
+++ src/core/runtime.js
@@ -0,0 +1,5 @@
+export default {
+  vue: {},
+  store: {},
+  router: {}
+}
--- src/core/vue.js
+++ src/core/vue.js
@@ -2,13 +2,18 @@ import Vue from 'vue'
 import store from './vuex'
 import router from './router'
 import AppComponent from '@/app'
+import dependencies from './runtime'
 import { Wait, Waiting } from '@/components/waiting'
 
 Vue.prototype.$wait = Wait
 Vue.prototype.$waiting = Waiting
 
-export const App = new Vue({
+const app = new Vue({
   store,
   router,
   render: h => h(AppComponent),
 }).$mount('#app')
+
+dependencies.vue = app
+dependencies.store = store
+dependencies.router = router
