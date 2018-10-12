ca729ba8a4fbdaca072bb99df642326cf4b24221
zhanglan <delylaric@gmail.com>
2018-10-13 00:38:50 +0800

    fix home page tag

--- src/app/home/index.vue
+++ src/app/home/index.vue
@@ -1,11 +1,7 @@
 <template>
-  <div style="display: flex; flex-flow: column; height: 100vh">
-    <div style="flex: 2; overflow: auto;">
-      <keep-alive>
-        <router-view />
-      </keep-alive>
-    </div>
-  </div>
+  <keep-alive>
+    <router-view />
+  </keep-alive>
 </template>
 
 <script>
