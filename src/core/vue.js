eae863c5ebd7946b8bd07cd8af3b2c4b9ba970f4
zhanglan <delylaric@gmail.com>
2018-10-16 06:26:25 +0800

    define global directive: v-clickoutside

--- src/core/vue.js
+++ src/core/vue.js
@@ -3,7 +3,10 @@ import store from './vuex'
 import router from './router'
 import AppComponent from '@/app'
 import dependencies from './runtime'
+import clickoutside from '@/directives/clickoutside'
 import { Wait, Waiting } from '@/components/waiting'
+
+Vue.directive('clickoutside', clickoutside)
 
 Vue.prototype.$wait = Wait
 Vue.prototype.$waiting = Waiting
